1ca12b77f744d161174a1fa6f3f686f45a1baaa4
Scott Lackey <thomasscottlackey@gmail.com>
2021-07-09 14:01:34 -0500

    fix problem with bundle not building since dirs_exist_okay not present in shutil.copytree

--- fw/bundle.py
+++ fw/bundle.py
@@ -1,13 +1,24 @@
-import os
-from shutil import copyfile, copytree
+import os, shutil
 from pathlib import Path
 
 platform_dirs = set([d for d in os.walk('.')][0][1])
 platform_dirs = platform_dirs - {'core', 'bundle'}
 
+#https://stackoverflow.com/a/12514470
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+
 for pd in platform_dirs:
     print(pd)
     path = Path(f"./bundle/{pd}")
     path.mkdir(parents=True, exist_ok=True)
-    copytree('core', path, dirs_exist_ok=True)
-    copytree(pd, path, dirs_exist_ok=True)
+    copytree("core", path)
+    copytree(pd, path)
+
